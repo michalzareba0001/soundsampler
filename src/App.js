be9f1377a082ad9e6f5e7257977a489efe31121c
Michał Zaręba <michalzareba0001@gmail.com>
2023-05-19 15:08:26 +0200

    25 working sound buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import SoundPad from './components/SoundPad'
 import Sample0 from './sounds/sample0.mp3'
 import Sample1 from './sounds/sample1.mp3'
 import Sample2 from './sounds/sample2.mp3'
+import Sample3 from './sounds/sample3.mp3'
+import Sample4 from './sounds/sample4.mp3'
+import Sample5 from './sounds/sample5.mp3'
+import Sample6 from './sounds/sample6.mp3'
+import Sample7 from './sounds/sample7.mp3'
+import Sample8 from './sounds/sample8.mp3'
+import Sample9 from './sounds/sample9.mp3'
+import Sample10 from './sounds/sample10.mp3'
+import Sample11 from './sounds/sample11.mp3'
+import Sample12 from './sounds/sample12.mp3'
+import Sample13 from './sounds/sample13.mp3'
+import Sample14 from './sounds/sample14.mp3'
+import Sample15 from './sounds/sample15.mp3'
+import Sample16 from './sounds/sample16.mp3'
+import Sample17 from './sounds/sample17.mp3'
+import Sample18 from './sounds/sample18.mp3'
+import Sample19 from './sounds/sample19.mp3'
+import Sample20 from './sounds/sample20.mp3'
+import Sample21 from './sounds/sample21.mp3'
+import Sample22 from './sounds/sample22.mp3'
+import Sample23 from './sounds/sample23.mp3'
+import Sample24 from './sounds/sample24.mp3'
 
 function App() {
   return (
@@ -15,6 +37,29 @@ function App() {
           sound1Url={Sample0}
           sound2Url={Sample1}
           sound3Url={Sample2}
+          sound4Url={Sample3}
+          sound5Url={Sample4}
+          sound6Url={Sample5}
+          sound7Url={Sample6}
+          sound8Url={Sample7}
+          sound9Url={Sample8}
+          sound10Url={Sample9}
+          sound11Url={Sample10}
+          sound12Url={Sample11}
+          sound13Url={Sample12}
+          sound14Url={Sample13}
+          sound15Url={Sample14}
+          sound16Url={Sample15}
+          sound17Url={Sample16}
+          sound18Url={Sample17}
+          sound19Url={Sample18}
+          sound20Url={Sample19}
+          sound21Url={Sample20}
+          sound22Url={Sample21}
+          sound23Url={Sample22}
+          sound24Url={Sample23}
+          sound25Url={Sample24}
+          
         />
       </div>
     </div>
--- src/components/SoundPad.js
+++ src/components/SoundPad.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import Sound from 'react-sound';
 
-const SoundPad = ({ sound1Url, sound2Url, sound3Url }) => {
+const SoundPad = ({ sound1Url,
+    sound2Url,
+    sound3Url,
+    sound4Url,
+    sound5Url,
+    sound6Url,
+    sound7Url,
+    sound8Url,
+    sound9Url,
+    sound10Url,
+    sound11Url,
+    sound12Url,
+    sound13Url,
+    sound14Url,
+    sound15Url,
+    sound16Url,
+    sound17Url,
+    sound18Url,
+    sound19Url,
+    sound20Url,
+    sound21Url,
+    sound22Url,
+    sound23Url,
+    sound24Url,
+    sound25Url }) => {
   const handlePlay = (url) => {
     const audio = new Audio(url);
     audio.play();
@@ -9,9 +32,32 @@ const SoundPad = ({ sound1Url, sound2Url, sound3Url }) => {
 
   return (
     <div className="sound-pad">
-      <button onClick={() => handlePlay(sound1Url)}>Sound 1</button>
-      <button onClick={() => handlePlay(sound2Url)}>Sound 2</button>
-      <button onClick={() => handlePlay(sound3Url)}>Sound 3</button>
+      <button className='sound-button' onClick={() => handlePlay(sound1Url)}>Sound 1</button>
+      <button className='sound-button' onClick={() => handlePlay(sound2Url)}>Sound 2</button>
+      <button className='sound-button' onClick={() => handlePlay(sound3Url)}>Sound 3</button>
+      <button className='sound-button' onClick={() => handlePlay(sound4Url)}>Sound 4</button>
+      <button className='sound-button' onClick={() => handlePlay(sound5Url)}>Sound 5</button>
+      <button className='sound-button' onClick={() => handlePlay(sound6Url)}>Sound 6</button>
+      <button className='sound-button' onClick={() => handlePlay(sound7Url)}>Sound 7</button>
+      <button className='sound-button' onClick={() => handlePlay(sound8Url)}>Sound 8</button>
+      <button className='sound-button' onClick={() => handlePlay(sound9Url)}>Sound 9</button>
+      <button className='sound-button' onClick={() => handlePlay(sound10Url)}>Sound 10</button>
+      <button className='sound-button' onClick={() => handlePlay(sound11Url)}>Sound 11</button>
+      <button className='sound-button' onClick={() => handlePlay(sound12Url)}>Sound 12</button>
+      <button className='sound-button' onClick={() => handlePlay(sound13Url)}>Sound 13</button>
+      <button className='sound-button' onClick={() => handlePlay(sound14Url)}>Sound 14</button>
+      <button className='sound-button' onClick={() => handlePlay(sound15Url)}>Sound 15</button>
+      <button className='sound-button' onClick={() => handlePlay(sound16Url)}>Sound 16</button>
+      <button className='sound-button' onClick={() => handlePlay(sound17Url)}>Sound 17</button>
+      <button className='sound-button' onClick={() => handlePlay(sound18Url)}>Sound 18</button>
+      <button className='sound-button' onClick={() => handlePlay(sound19Url)}>Sound 19</button>
+      <button className='sound-button' onClick={() => handlePlay(sound20Url)}>Sound 20</button>
+      <button className='sound-button' onClick={() => handlePlay(sound21Url)}>Sound 21</button>
+      <button className='sound-button' onClick={() => handlePlay(sound22Url)}>Sound 22</button>
+      <button className='sound-button' onClick={() => handlePlay(sound23Url)}>Sound 23</button>
+      <button className='sound-button' onClick={() => handlePlay(sound24Url)}>Sound 24</button>
+      <button className='sound-button' onClick={() => handlePlay(sound25Url)}>Sound 25</button>
+      
     </div>
   );
 };
